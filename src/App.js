668eab224eb811cb06a710732585f3fdec028876
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-05-15 23:22:50 +0300

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import store from './redux/store';
 import Navbar from './components/Navbar';
 import Profile from './pages/Profile';
 import Rocket from './pages/Rocket';
@@ -8,14 +10,16 @@ import Mission from './pages/Mission';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Rocket />} />
-        <Route exact path="/missions" element={<Mission />} />
-        <Route exact path="/profile" element={<Profile />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Rocket />} />
+          <Route exact path="/missions" element={<Mission />} />
+          <Route exact path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
