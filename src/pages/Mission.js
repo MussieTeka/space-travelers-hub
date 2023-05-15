fe45a8f2f50b5f2d60a3890e398d67e34542f333
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-05-15 23:23:15 +0300

    Modify Mission.js

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissions, selectMissions } from '../redux/mission/missionSlice';
 
-const Mission = () => (
-  <div>
-    This is the Mission page.
-  </div>
-);
+const Mission = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector(selectMissions);
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {missions.map((mission) => (
+        <div key={mission.mission_id}>
+          <h2>{mission.mission_name}</h2>
+          <p>{mission.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Mission;
