2abd831e50e7e3e65802252af2724e0c2d177cae
skabeo <spencerokyere95@gmail.com>
2023-05-15 14:36:00 +0100

    add rocket route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Profile from './pages/Profile';
+import Rocket from './pages/Rocket';
+
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route exact path="/" element={<Rocket />} />
         <Route exact path="/profile" element={<Profile />} />
       </Routes>
     </Router>
