c54be309f8bb6c957b177845462e23341d2e6bfd
skabeo <spencerokyere95@gmail.com>
2023-05-16 18:37:04 +0100

    Modify  src/components/RocketList.js file

--- src/components/RocketList.js
+++ src/components/RocketList.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import styles from '../styles/components/Rocket.module.scss';
+import { reserveRocket } from '../redux/rocket/rocketSlice';
 
-const RocketList = ({ name, image, description }) => (
-  <div className={styles.container}>
-    <div className={styles.imgContainer}>
-      <img src={image} alt={name} />
+const RocketList = ({
+  id, name, image, description,
+}) => {
+  const dispatch = useDispatch();
+
+  const handleReserve = () => {
+    dispatch(reserveRocket({ id }));
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <img src={image} alt={name} />
+      </div>
+      <div className={styles.infoHolder}>
+        <h3 style={{ paddingBottom: '10px' }}>{name}</h3>
+        <p style={{ paddingBottom: '10px' }}>{description}</p>
+        <button
+          type="button"
+          className={styles.notReserved}
+          onClick={handleReserve}
+        >
+          Reserve Rocket
+        </button>
+      </div>
     </div>
-    <div className={styles.infoHolder}>
-      <h3 style={{ paddingBottom: '10px' }}>{name}</h3>
-      <p style={{ paddingBottom: '10px' }}>{description}</p>
-      <button
-        type="button"
-        className={styles.notReserved}
-      >
-        Reserve Rocket
-      </button>
-    </div>
-  </div>
-);
+  );
+};
 
 RocketList.propTypes = {
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
