4aba0c9fdb04cc8a7fd03bf9f0a0a84ece1d80a6
skabeo <spencerokyere95@gmail.com>
2023-05-16 15:51:38 +0100

    Display rocket data

---
+++ src/components/RocketItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import RocketList from './RocketList';
+
+const RocketItem = () => {
+  const rockets = useSelector((state) => state.rockets.rocket);
+
+  return (
+    <div>
+      {rockets.map((rocket) => (
+        <RocketList
+          key={rocket.id}
+          name={rocket.name}
+          image={rocket.image}
+          description={rocket.description}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default RocketItem;
