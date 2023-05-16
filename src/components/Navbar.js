98f42cac8c135161ead3a61f504f3bf1a980dace
skabeo <spencerokyere95@gmail.com>
2023-05-16 12:37:39 +0100

    Modify navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,24 +7,22 @@ const Navbar = () => (
   <div>
     <nav>
       <div>
-        <NavLink exact to="/">
-          <img className={styles.logo} src={planet} alt="logo" />
-        </NavLink>
+        <img className={styles.logo} src={planet} alt="logo" />
         <h1>Space Travelers&apos; Hub</h1>
       </div>
       <ul>
         <li>
-          <NavLink exact to="/" activeClassName={styles.active}>
+          <NavLink exact="true" to="/">
             Rockets
           </NavLink>
         </li>
         <li>
-          <NavLink exact to="/missions" activeClassName={styles.active}>
+          <NavLink exact="true" to="/missions">
             Missions
           </NavLink>
         </li>
         <li>
-          <NavLink exact to="/profile" activeClassName={styles.active}>
+          <NavLink exact="true" to="/profile">
             My Profile
           </NavLink>
         </li>
