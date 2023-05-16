3806906eb3a5a6fa34f7470b0209c887b8d3609b
skabeo <spencerokyere95@gmail.com>
2023-05-16 12:36:57 +0100

    Modify     App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Navbar from './components/Navbar';
 import Profile from './pages/Profile';
 import Rocket from './pages/Rocket';
 import Mission from './pages/Mission';
 import { fetchMissions } from './redux/mission/missionSlice';
+import { fetchRockets } from './redux/rocket/rocketSlice';
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchMissions());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchRockets())
   }, [dispatch]);
 
   return (
