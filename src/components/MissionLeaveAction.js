82dd70d3ff4ab269e26aab94a5bd2c1887646879
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-05-17 04:44:14 +0300

    Add MissionLeaveAction.js

---
+++ src/components/MissionLeaveAction.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { leaveMission } from '../redux/mission/missionSlice';
+import '../styles/components/MissionLeaveAction.css';
+
+const MissionLeaveAction = ({ missionId }) => {
+  const dispatch = useDispatch();
+
+  const handleLeaveMission = () => {
+    dispatch(leaveMission(missionId));
+  };
+
+  return (
+    <button type="button" className="mission-leave-button" onClick={handleLeaveMission}>Leave Mission</button>
+  );
+};
+
+MissionLeaveAction.propTypes = {
+  missionId: PropTypes.string.isRequired,
+};
+
+export default MissionLeaveAction;
