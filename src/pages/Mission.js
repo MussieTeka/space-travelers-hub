bf21572934bb9226806d5e4bb47bed4d72c0255c
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-05-17 04:42:36 +0300

    Modify Mission.js

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectMissions } from '../redux/mission/missionSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectMissions, joinMission, leaveMission } from '../redux/mission/missionSlice';
 import '../styles/pages/Mission.css';
 import MissionAction from '../components/MissionAction';
+import MissionLeaveAction from '../components/MissionLeaveAction';
 
 const Mission = () => {
   const missions = useSelector(selectMissions);
+  const dispatch = useDispatch();
+
+  const handleJoinMission = (missionId) => {
+    dispatch(joinMission(missionId));
+  };
+
+  const handleLeaveMission = (missionId) => {
+    dispatch(leaveMission(missionId));
+  };
 
   return (
     <div>
@@ -14,7 +24,7 @@ const Mission = () => {
           <tr>
             <th>Mission</th>
             <th>Description</th>
-            <th>Action</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -23,7 +33,17 @@ const Mission = () => {
               <td>{mission.mission_name}</td>
               <td>{mission.description}</td>
               <td>
-                <MissionAction missionId={mission.mission_id} />
+                {mission.reserved ? (
+                  <MissionLeaveAction
+                    missionId={mission.mission_id}
+                    onLeaveMission={handleLeaveMission}
+                  />
+                ) : (
+                  <MissionAction
+                    missionId={mission.mission_id}
+                    onJoinMission={handleJoinMission}
+                  />
+                )}
               </td>
             </tr>
           ))}
