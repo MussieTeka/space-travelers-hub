d31f6dde36b8241fdcac74ef5d0654d81f57d298
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-05-17 13:14:49 +0300

    Add NotMemberButton.js

---
+++ src/components/NotMemberButton.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../styles/components/NotMemberButton.css';
+
+const NotMemberButton = () => <button type="button" className="not-member-button">NOT A MEMBER</button>;
+
+export default NotMemberButton;
