558c4e3ba657b66e408aea9d7643d73cb2d1781a
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-05-17 13:13:40 +0300

    Modify Mission.js

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectMissions, joinMission, leaveMission } from '../redux/mission/missionSlice';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { selectMissions } from '../redux/mission/missionSlice';
 import '../styles/pages/Mission.css';
 import MissionAction from '../components/MissionAction';
 import MissionLeaveAction from '../components/MissionLeaveAction';
+import ActiveMemberButton from '../components/ActiveMemberButton';
+import NotMemberButton from '../components/NotMemberButton';
+
+const MemberStatus = ({ reserved }) => (
+  <td>{reserved ? <ActiveMemberButton /> : <NotMemberButton />}</td>
+);
+
+MemberStatus.propTypes = {
+  reserved: PropTypes.bool.isRequired,
+};
 
 const Mission = () => {
   const missions = useSelector(selectMissions);
-  const dispatch = useDispatch();
-
-  const handleJoinMission = (missionId) => {
-    dispatch(joinMission(missionId));
-  };
-
-  const handleLeaveMission = (missionId) => {
-    dispatch(leaveMission(missionId));
-  };
 
   return (
     <div>
@@ -24,6 +26,7 @@ const Mission = () => {
           <tr>
             <th>Mission</th>
             <th>Description</th>
+            <th>Status</th>
             <th>Actions</th>
           </tr>
         </thead>
@@ -32,17 +35,12 @@ const Mission = () => {
             <tr key={mission.mission_id}>
               <td>{mission.mission_name}</td>
               <td>{mission.description}</td>
+              <MemberStatus reserved={mission.reserved} />
               <td>
                 {mission.reserved ? (
-                  <MissionLeaveAction
-                    missionId={mission.mission_id}
-                    onLeaveMission={handleLeaveMission}
-                  />
+                  <MissionLeaveAction missionId={mission.mission_id} />
                 ) : (
-                  <MissionAction
-                    missionId={mission.mission_id}
-                    onJoinMission={handleJoinMission}
-                  />
+                  <MissionAction missionId={mission.mission_id} />
                 )}
               </td>
             </tr>
