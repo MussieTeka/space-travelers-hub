6cc7900f9f1ecf6a6b0d453893165b9b7264fe4c
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-05-18 13:25:26 +0300

    Add __tests__ directory.

--- src/__tests__/ActiveMemberButton.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import ActiveMemberButton from '../components/ActiveMemberButton';
-
-describe('ActiveMemberButton', () => {
-  test('renders without errors', () => {
-    render(<ActiveMemberButton />);
-  });
-});
--- src/__tests__/NotMemberButton.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import NotMemberButton from '../components/NotMemberButton';
-
-describe('NotMemberButton', () => {
-  test('renders without errors', () => {
-    render(<NotMemberButton />);
-  });
-});
